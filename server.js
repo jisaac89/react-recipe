a008fe10070d4307abca477fd588db3954c841da
jisaac89 <josephisaac89@gmail.com>
2018-02-17 16:42:38 -0800

    added sample sever js, next now init with express

--- server.js
+++ server.js
@@ -6,24 +6,23 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-    .then(() => {
-        const server = express()
+app.prepare().then(() => {
+    const server = express()
 
-        server.get('/about', (req, res) => {
-            return app.render(req, res, '/about', req.query)
-        })
+    server.get('/about', (req, res) => {
+        return app.render(req, res, '/about', req.query)
+    })
 
-        server.get('/index', (req, res) => {
-            return app.render(req, res, '/index', req.query)
-        })
+    server.get('/index', (req, res) => {
+        return app.render(req, res, '/index', req.query)
+    })
 
-        server.get('*', (req, res) => {
-            return handle(req, res)
-        })
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    })
 
-        server.listen(port, (err) => {
-            if (err) throw err
-            console.log(`> Ready on http:// localhost:${port}`)
-        })
-    });
+    server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http:// localhost:${port}`)
+    })
+});
