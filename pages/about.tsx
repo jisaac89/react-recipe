0bb679c3ad4856a78e6fc6c63a4527642bdc62e7
jisaac89 <josephisaac89@gmail.com>
2018-02-17 16:23:45 -0800

    first push

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class About extends React.Component<any, any>{
+  render() {
+    return (
+      <div>
+        About
+      </div>
+    )
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class Index extends React.Component<any, any>{
+  render() {
+    return (
+      <div>
+        Index
+      </div>
+    )
+  }
+}
